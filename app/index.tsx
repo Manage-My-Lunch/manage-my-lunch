629bd49fb84452a7f8653f880a96f8a691a398dd
mangorioh <104879428+mangorioh@users.noreply.github.com>
2024-10-10 10:23:52 +1300

    init commit
    -forgot password page
    -reset password page
    -navigate to those pages from home
    
    todo:
    test and verify backend functions

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,37 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <View style={styles.container}>
+    <Pressable style={styles.button} onPress={() => router.push("/forgot-password")}>
+      <Text style={styles.buttonText}>Forgot Password</Text>
+    </Pressable>
+    <Pressable style={styles.button} onPress={() => router.push("/reset-password")}>
+      <Text style={styles.buttonText}>Reset Password</Text>
+    </Pressable>
+  </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#00BFA6",
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
--- app/reset-password/index.tsx
+++ app/reset-password/index.tsx
@@ -1,15 +1,86 @@
-import { Text, View } from "react-native";
+import { TextInput, Text, View, StyleSheet, Pressable, Alert } from "react-native";
+import { useState } from "react";
+import { supabase } from "@/lib/supabase";
 
 export default function Index() {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleReset = async () => {
+    if (password !== confirmPassword) {
+      Alert.alert("Passwords don't match.");
+      return;
+    }
+
+    // TODO: i need to actually text this xd
+    const { data, error } = await supabase.auth.updateUser({
+      password: password
+    })
+
+    if (error) {
+      alert("Error" + error.message);
+    } else {
+      alert("Password updated!");
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Reset Password</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry={true}
+        autoCapitalize="none"
+      />
+      
+      <TextInput
+        style={styles.input}
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        onChangeText={(text) => setConfirmPassword(text)}
+        secureTextEntry={true}
+        autoCapitalize="none"
+      />
+
+      <Pressable style={styles.button} onPress={handleReset}>
+        <Text style={styles.buttonText}>UpdatePassword</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#00BFA6",
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  input: {
+    width: "80%",
+    padding: 12,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+  },
+  text: {
+    padding: 12,
+    fontSize: 18,
+  }
+});
