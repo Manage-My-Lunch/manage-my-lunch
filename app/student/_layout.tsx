4604780d04075a2556df683deaeecd1fb08fea4d
TheRangiCrew <ryan.z.adam@gmail.com>
2025-03-23 13:23:25 +1300

    Add cart provider

--- app/student/_layout.tsx
+++ app/student/_layout.tsx
@@ -1,43 +1,47 @@
-import { Platform } from "react-native";
-import React from "react";
+import { Platform, View, Text, ActivityIndicator, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
 import { Tabs } from "expo-router";
 import { Entypo, Ionicons } from "@expo/vector-icons";
+import { Cart, CartProvider } from "@/lib/cart";
 
 const TabsLayout = () => {
-  return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: "#00BFA6",
-        tabBarInactiveTintColor: "#gray",
-        tabBarStyle: {
-          position: Platform.OS === "web" ? "absolute" : "relative",
-          bottom: Platform.OS === "web" ? "auto" : 0,
-          top: Platform.OS === "web" ? 0 : "auto",
-          backgroundColor: "#F7F9FC",
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="menu"
-        options={{
-          tabBarLabel: "Home",
-          tabBarIcon: ({ color }) => (
-            <Entypo name="home" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          tabBarLabel: "Profile",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="person" size={24} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <CartProvider>
+            <Tabs
+                screenOptions={{
+                    headerShown: false,
+                    tabBarActiveTintColor: "#00BFA6",
+                    tabBarInactiveTintColor: "#gray",
+                    tabBarStyle: {
+                        position:
+                            Platform.OS === "web" ? "absolute" : "relative",
+                        bottom: Platform.OS === "web" ? "auto" : 0,
+                        top: Platform.OS === "web" ? 0 : "auto",
+                        backgroundColor: "#F7F9FC",
+                    },
+                }}
+            >
+                <Tabs.Screen
+                    name="menu"
+                    options={{
+                        tabBarLabel: "Home",
+                        tabBarIcon: ({ color }) => (
+                            <Entypo name="home" size={24} color={color} />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="profile"
+                    options={{
+                        tabBarLabel: "Profile",
+                        tabBarIcon: ({ color }) => (
+                            <Ionicons name="person" size={24} color={color} />
+                        ),
+                    }}
+                />
+            </Tabs>
+        </CartProvider>
+    );
 };
 
 export default TabsLayout;
